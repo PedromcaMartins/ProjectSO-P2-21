7be1bf493499e2e962b0af5f9702846e49448e03
PipesAkaTubinhos <pipo.martins.27.06@gmail.com>
2022-02-01 11:43:45 +0000

    reads input to get server pipe

--- tests/simple_client.c
+++ tests/simple_client.c
@@ -12,13 +12,20 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define SERVER_PATHNAME "/tmp/server"
 #define CLIENT_PATHNAME "/tmp/acks-pipe"
 #define MAX_SIZE_PATHNAME 40
 
-int main() {
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        printf("Please specify the pathname of the server's pipe.\n");
+        return 1;
+    }
+
+    char *server_pathname = argv[1];
+    printf("Starting TecnicoFS server with pipe called %s\n", server_pathname);
+
     char client_pathname[MAX_SIZE_PATHNAME] = CLIENT_PATHNAME;
-    char server_pathname[MAX_SIZE_PATHNAME] = SERVER_PATHNAME;
 
     assert(tfs_mount(client_pathname, server_pathname) != -1);
 
