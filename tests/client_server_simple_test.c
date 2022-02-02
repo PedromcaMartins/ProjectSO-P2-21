766ae77e04947f1a61e43745c5995fb1d4b220db
PipesAkaTubinhos <pipo.martins.27.06@gmail.com>
2022-02-02 16:36:36 +0000

    added git issue

--- tests/client_server_simple_test.c
+++ tests/client_server_simple_test.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
+//TODO: #5 FIX este teste
 
 /*  This test is similar to test1.c from the 1st exercise.
     The main difference is that this one explores the
